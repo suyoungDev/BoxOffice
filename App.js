abe272074eee3abf8609e42a85234760bc1d824f
mochapoke <71932072+mochapoke@users.noreply.github.com>
2020-11-16 18:43:16 +0900

    추가:네비게이션

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import BoxOffice from './pages/BoxOffice';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#ffffff',
+  },
+};
 
 const App: () => React$Node = () => {
   return (
-   <>
-   <BoxOffice />
-   </> 
+    <>
+      <NavigationContainer theme={Theme}>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="Box Office" 
+            component={BoxOffice} 
+            options={{headerShown:false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );  
 };
 
--- pages/BoxOffice.js
+++ pages/BoxOffice.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ActivityIndicator} from 'react-native'
 import styled from 'styled-components';
 import axios from 'axios';
 
@@ -6,29 +7,33 @@ const Container = styled.SafeAreaView`
   flex:1;
 `;
 const Contents = styled.ScrollView`
-  padding: 20px;
+  padding: 9px;
 `;
 const Rank = styled.Text`
   font-size: 14px;
   font-weight: bold;
-  color: #999999;
-  margin-right: 12px;
+  color: #bcaaa4;
+  margin-right: 20px;
 `;
 const Title = styled.Text`
-  font-size: 24px;
+  font-size: 29px;
   font-weight: bold;
-  letter-spacing: 3px;
+  letter-spacing: 6px;
+  padding: 10px 0px;
+  color: #8c7b75;
 `;
 const ListItem = styled.TouchableOpacity`
-  padding: 12px;
+  padding: 11px 20px 11px 10px;
   border-bottom-color: #e5e5e5;
   border-bottom-width: 1px;
   flex-direction: row;
   align-items: center;
+
 `;
 const MovieName= styled.Text`
   font-size: 18px;
   font-weight: bold;
+  padding: 2px 0px;
 `;
 
 
@@ -37,7 +42,7 @@ function BoxOffice(props){
 
   React.useEffect(() => {
     // ajax 비동기 자바스크립트 XML(JSON)
-    axios.get('https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=ea9876218d7764c2894c4270a491e31b&targetDt=20201116')
+    axios.get('http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=ea9876218d7764c2894c4270a491e31b&targetDt=20201114')
       .then( response => {
         // 완료가 됐을 때
         setList( response.data.boxOfficeResult.dailyBoxOfficeList );
@@ -51,11 +56,14 @@ function BoxOffice(props){
   return(
     <Container>
       <Contents>
-        <Title>박스 오피스</Title>
+        <Title>Box Office</Title>
+        { list.length === 0 && (
+              <ActivityIndicator size="large" color="#0000ff" />
+        )}
         {list.map( elem => (
           <ListItem key={ elem.movieCd }>
             <Rank>{ elem.rank }</Rank>
-            <MovieName>{ elem.MovieNm }</MovieName>
+            <MovieName>{ elem.movieNm }</MovieName>
           </ListItem>
         ))}
       </Contents>
